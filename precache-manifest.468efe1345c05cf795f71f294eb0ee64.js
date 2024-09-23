5e14490ac1ad9cef2f0e68cdf74d4131480bcaff
Oleksandr Pelykh <pelykholeksander@gmail.com>
2024-09-23 15:03:38 +0300

    Updates

---
+++ precache-manifest.468efe1345c05cf795f71f294eb0ee64.js
@@ -0,0 +1,134 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "918f25b11cc2840da29083ebf150bb87",
+    "url": "/project__article-haven/index.html"
+  },
+  {
+    "revision": "f298d917ab5f78731cd8",
+    "url": "/project__article-haven/static/css/main.f77fa732.chunk.css"
+  },
+  {
+    "revision": "a2f1f13c4306ad01dd8b",
+    "url": "/project__article-haven/static/js/2.d8aded79.chunk.js"
+  },
+  {
+    "revision": "f298d917ab5f78731cd8",
+    "url": "/project__article-haven/static/js/main.bae15ce0.chunk.js"
+  },
+  {
+    "revision": "73900b75bd3084427bfb",
+    "url": "/project__article-haven/static/js/runtime~main.54d7f9c8.js"
+  },
+  {
+    "revision": "5727678374af5b7b42348af1ad4b1199",
+    "url": "/project__article-haven/static/media/add.57276783.png"
+  },
+  {
+    "revision": "ecc5be02df9f536dd709d8ed1e894aee",
+    "url": "/project__article-haven/static/media/altImg.ecc5be02.png"
+  },
+  {
+    "revision": "e7a9bb2568d325b6c80d3ca485d6b89d",
+    "url": "/project__article-haven/static/media/arrow-btn-2.e7a9bb25.png"
+  },
+  {
+    "revision": "7ce7c8d06dc7fd4ff2d4e62a9a4609ac",
+    "url": "/project__article-haven/static/media/arrow.7ce7c8d0.png"
+  },
+  {
+    "revision": "c1e6c88b9cfecdabafb2cbf689bee051",
+    "url": "/project__article-haven/static/media/book-logo.c1e6c88b.png"
+  },
+  {
+    "revision": "4bb455d86cb0620b75db1ab80b4b2bb0",
+    "url": "/project__article-haven/static/media/book.4bb455d8.png"
+  },
+  {
+    "revision": "c5658c09668c9f3994e0480887883160",
+    "url": "/project__article-haven/static/media/date.c5658c09.png"
+  },
+  {
+    "revision": "99718fc85839cc30c417fcab78a2edb8",
+    "url": "/project__article-haven/static/media/dislike.99718fc8.png"
+  },
+  {
+    "revision": "0905ac04b0dc94770d2511279856cfe0",
+    "url": "/project__article-haven/static/media/empty.0905ac04.png"
+  },
+  {
+    "revision": "1b9b9f71269e504156ce9d89a00f2551",
+    "url": "/project__article-haven/static/media/git.1b9b9f71.png"
+  },
+  {
+    "revision": "48c857a89fa5d4f6e1d1317049729322",
+    "url": "/project__article-haven/static/media/help.48c857a8.png"
+  },
+  {
+    "revision": "8290266a5fb402a34b96f890bbdb2d60",
+    "url": "/project__article-haven/static/media/inst.8290266a.png"
+  },
+  {
+    "revision": "96814bb1040e7fb89edc8abfd2de5689",
+    "url": "/project__article-haven/static/media/like.96814bb1.png"
+  },
+  {
+    "revision": "941b8eabc43651720692aecc52305bbb",
+    "url": "/project__article-haven/static/media/linkedIn.941b8eab.png"
+  },
+  {
+    "revision": "d776811e4b880b009df1d5653dbce379",
+    "url": "/project__article-haven/static/media/news.d776811e.png"
+  },
+  {
+    "revision": "f2cdcc53ef85542d4fa74c248db6ef49",
+    "url": "/project__article-haven/static/media/pen.f2cdcc53.png"
+  },
+  {
+    "revision": "bf2db3b76b9545884b68c46f2381e645",
+    "url": "/project__article-haven/static/media/planet.bf2db3b7.png"
+  },
+  {
+    "revision": "f4c743c5e75c29e254bf52d15f7da42e",
+    "url": "/project__article-haven/static/media/plus.f4c743c5.png"
+  },
+  {
+    "revision": "06b031c29c889e7a92dafa0f5a413bcc",
+    "url": "/project__article-haven/static/media/post-image-1.06b031c2.jpg"
+  },
+  {
+    "revision": "afb54c4a7e84587cd248e767b156962d",
+    "url": "/project__article-haven/static/media/post-image-2.afb54c4a.jpg"
+  },
+  {
+    "revision": "b4630fcb5f7ec496b6a7bdeadea7c4a4",
+    "url": "/project__article-haven/static/media/post-image-3.b4630fcb.jpg"
+  },
+  {
+    "revision": "e99cfa845bdd9c6631f2c755b3c887bc",
+    "url": "/project__article-haven/static/media/post-image-4.e99cfa84.jpg"
+  },
+  {
+    "revision": "11aa8aec12197a4ee2c58e70e8f484cd",
+    "url": "/project__article-haven/static/media/search.11aa8aec.png"
+  },
+  {
+    "revision": "88dde1cdfad19a639bf79ef96a50eaa5",
+    "url": "/project__article-haven/static/media/sport.88dde1cd.png"
+  },
+  {
+    "revision": "a5d554848a460252afd021026c1ba83e",
+    "url": "/project__article-haven/static/media/study.a5d55484.png"
+  },
+  {
+    "revision": "aab54da45d735d2cf0f24b42f36c9f98",
+    "url": "/project__article-haven/static/media/test4.aab54da4.mp4"
+  },
+  {
+    "revision": "b37d6df3b36a74748a9bda890b3ba550",
+    "url": "/project__article-haven/static/media/tg.b37d6df3.png"
+  },
+  {
+    "revision": "a11cd8c4fc27cd92632ab1e4956d2bbb",
+    "url": "/project__article-haven/static/media/title.a11cd8c4.png"
+  }
+]);
